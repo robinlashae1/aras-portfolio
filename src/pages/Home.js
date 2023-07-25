d98afe06a60016f69047c22ad1881f4fedf5f726
Robin Vernon <robinlashae1@gmail.com>
2023-07-24 22:29:38 -0400

    main home section complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 
 import DisplayCards from "../additional-comps/DisplayCards";
 import checkOutMyRecentWork from "../img/checkOutMyRecentWork 1.png"
+import arrow from "../img/arrow.png"
 function Home({projects}){
 	console.log(projects)
 
@@ -24,9 +25,9 @@ function Home({projects}){
 				</div>
 				
 				
-				<div id="home-button-section">
+				<div id="home-button-section" className="flex-column">
 					<img src={checkOutMyRecentWork}/>
-					<svg id="home-arrow" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M11 21.883l-6.235-7.527-.765.644 7.521 9 7.479-9-.764-.645-6.236 7.529v-21.884h-1v21.883z"/></svg>
+					<img src={arrow} />
 				</div>
 			</section>
 
